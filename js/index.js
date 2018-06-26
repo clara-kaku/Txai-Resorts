7e972843eeb084bc434c046a4dbb143915ac34b8
clara kaku <clara.kaku@embraer.com.br>
2018-06-25 23:14:47 -0300

    First commit
    Txai resorts website

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke'],
+        navigation: true,
+        navigationTooltips: ['1', '2', '3', '4'],
+        navigationPosition: 'left'
+    });
+
+    //$('#fp-nav.left').wrap('<div class="side-menu"> OIIIIIII </div>');
+});
